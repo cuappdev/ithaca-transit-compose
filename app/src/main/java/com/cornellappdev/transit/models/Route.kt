e5fc7a4a713559de97ddb296317d686e5191053e
Jonathan Chen <61638222+04jono@users.noreply.github.com>
2024-02-20 13:07:31 -0500

    home page low fidelity (#1)
    * home page low fidelity
    
    * Added placeholders in viewmodel
    
    * added specs

---
+++ app/src/main/java/com/cornellappdev/transit/models/Route.kt
@@ -0,0 +1,8 @@
+package com.cornellappdev.transit.models
+
+/**
+ * Data class representing routes on the map
+ */
+data class Route (
+    val name: String
+)
---
+++ app/src/main/java/com/cornellappdev/transit/models/RouteRepository.kt
@@ -0,0 +1,11 @@
+package com.cornellappdev.transit.models
+
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Repository for data related to routes
+ */
+object RouteRepository {
+    //TODO
+
+}
