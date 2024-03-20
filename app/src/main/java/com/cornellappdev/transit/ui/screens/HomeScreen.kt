7ca682b253ba2a7efbf4a3f2e753360e14f7b158
Jonathan Chen <jqc3@cornell.edu>
2024-03-20 17:15:47 -0400

    state in

--- app/src/main/java/com/cornellappdev/transit/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/screens/HomeScreen.kt
@@ -116,7 +116,7 @@ fun HomeScreen(
 
     // Collect flow of rides through API
     val stopsApiResponse = homeViewModel.stopFlow.collectAsState().value
-    val queryResponse = homeViewModel.queryFlow.collectAsState(initial = emptyList()).value
+    val queryResponse = homeViewModel.queryFlow.collectAsState().value
     val placesResponse = homeViewModel.placeData.collectAsState().value
 
     //Collect flow of route through API
--- app/src/main/java/com/cornellappdev/transit/ui/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/viewmodels/HomeViewModel.kt
@@ -15,10 +15,12 @@ import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -76,7 +78,11 @@ class HomeViewModel @Inject constructor(
     /**
      * Search query filtered flow of all TCAT stops
      */
-    val queryFlow = _queryFlow
+    val queryFlow = _queryFlow.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.Eagerly,
+        initialValue = emptyList()
+    )
 
 
     /**
