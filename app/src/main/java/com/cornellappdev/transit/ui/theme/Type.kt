2778232e51611a6e7611e667421765d890d22db8
Jonathan Chen <jqc3@cornell.edu>
2024-03-20 17:34:23 -0400

    missing import

--- app/src/main/java/com/cornellappdev/transit/ui/theme/Type.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/theme/Type.kt
@@ -2,6 +2,7 @@ package com.cornellappdev.transit.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
