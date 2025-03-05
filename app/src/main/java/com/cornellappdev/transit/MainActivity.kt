2fb8d23cf2106156545446a039192f8b073f1912
Jonathan Chen <04jono@gmail.com>
2025-03-05 17:25:46 -0500

    neat disable dark mode

--- app/src/main/java/com/cornellappdev/transit/MainActivity.kt
+++ app/src/main/java/com/cornellappdev/transit/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TransitTheme {
+            TransitTheme(darkTheme = false) {
                 NavigationController()
             }
         }
