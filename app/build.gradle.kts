bd8c1d46f9ab2673a229b0e477480614a8d16364
zachseidner1 <zachary.seidner@gmail.com>
2025-03-06 12:32:14 -0500

    Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.cornellappdev.transit"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
