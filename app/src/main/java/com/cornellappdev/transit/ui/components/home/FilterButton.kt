db041a6ac83bc0155632f58f5232f5b48bfd8b1f
H2ODERP <110103899+H2ODERP@users.noreply.github.com>
2026-02-07 02:20:59 -0500

    Updated filter icon file

--- app/src/main/java/com/cornellappdev/transit/ui/components/home/FilterButton.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/components/home/FilterButton.kt
@@ -1,17 +1,27 @@
 package com.cornellappdev.transit.ui.components.home
 
 import android.R.attr.text
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cornellappdev.transit.R
 
 @Composable
 fun FilterButton(){
     Button(onClick = {}){
-        Icon(painter = androidx.compose.ui.res.painterResource(id = com.cornellappdev.transit.R.drawable.filter_icon), contentDescription = "Filter Icon")
-        Text(text = "Filter")
+        Icon(
+            painter = painterResource(R.drawable.filter_icon),
+            contentDescription = "Filter Icon",
+            modifier = Modifier.size(20.dp)
+        )
+        Text(text = "Filter", fontSize = 16.sp)
     }
 }
 
