9c5faca24e2e9e5bd678f12e05e2e7be257bac96
H2ODERP <110103899+H2ODERP@users.noreply.github.com>
2026-02-08 02:09:10 -0500

    feat: Add plus icon to add favorites button

--- app/src/main/java/com/cornellappdev/transit/ui/components/home/AddFavoriteButton.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/components/home/AddFavoriteButton.kt
@@ -1,18 +1,22 @@
 package com.cornellappdev.transit.ui.components.home
 
+import android.R.attr.contentDescription
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.cornellappdev.transit.R
 import com.cornellappdev.transit.ui.theme.SecondaryText
 import com.cornellappdev.transit.ui.theme.robotoFamily
 
@@ -29,7 +33,14 @@ fun AddFavoriteButton(
             .padding(horizontal = 12.dp, vertical = 20.dp)
             .height(40.dp)
     ) {
-        //TODO: Add '+' Icon
+
+        Icon(
+            painter = painterResource(R.drawable.ic_addition),
+            contentDescription = "Add Favorite",
+            tint = SecondaryText,
+            modifier = Modifier.padding(end = 8.dp)
+            )
+
         Text(
             text ="Add Favorites",
             fontFamily = robotoFamily,
