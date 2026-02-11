07ba239dcdd00909ca1c61ce5d4a4feb7641efb1
Ryan Cheung <110103899+RyanCheung555@users.noreply.github.com>
2026-02-11 16:52:13 -0500

    fix: make AddFavoriteButton preview private

--- app/src/main/java/com/cornellappdev/transit/ui/components/home/AddFavoriteButton.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/components/home/AddFavoriteButton.kt
@@ -55,7 +55,7 @@ fun AddFavoriteButton(
 
 @Preview
 @Composable
-fun AddFavoriteButtonPreview() {
+private fun AddFavoriteButtonPreview() {
     AddFavoriteButton(
         onAddFavoriteClick = {},
     )
