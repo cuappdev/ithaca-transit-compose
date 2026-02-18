57ed6cd6dbe89671b8367aa6c86b7df7d72afd4b
Ryan Cheung <110103899+RyanCheung555@users.noreply.github.com>
2026-02-18 01:06:31 -0500

    feat: add correct icons for filter bottom sheet

--- app/src/main/java/com/cornellappdev/transit/ui/viewmodels/FavoritesFilterSheetState.kt
+++ app/src/main/java/com/cornellappdev/transit/ui/viewmodels/FavoritesFilterSheetState.kt
@@ -2,12 +2,10 @@ package com.cornellappdev.transit.ui.viewmodels
 
 import androidx.annotation.DrawableRes
 import com.cornellappdev.transit.R
-
-//TODO: Use Icons without background
 enum class FavoritesFilterSheetState(@DrawableRes val iconId: Int, val label: String) {
-    PRINTERS(iconId = R.drawable.printer_icon, label = "Printers"),
-    GYMS(iconId = R.drawable.gym_icon, label = "Gyms"),
-    EATERIES(iconId = R.drawable.eatery_icon, label = "Eateries"),
-    LIBRARIES(iconId = R.drawable.library_icon, label = "Libraries"),
-    OTHER(iconId = R.drawable.favorites_icon, label = "Other"),
+    PRINTERS(iconId = R.drawable.printer_filter_icon, label = "Printers"),
+    GYMS(iconId = R.drawable.gym_filter_icon, label = "Gyms"),
+    EATERIES(iconId = R.drawable.eatery_filter_icon, label = "Eateries"),
+    LIBRARIES(iconId = R.drawable.library_filter_icon, label = "Libraries"),
+    OTHER(iconId = R.drawable.other_filter_icon, label = "Other"),
 }
