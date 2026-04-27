f1a2fb473bd3a57246ec60f9eeb1b562c6ffb447
AbigailLabanok <113201330+AbigailLabanok@users.noreply.github.com>
2026-04-26 20:49:24 -0400

    fixed minor issues in intentUtils

--- app/src/main/java/com/cornellappdev/transit/util/IntentUtils.kt
+++ app/src/main/java/com/cornellappdev/transit/util/IntentUtils.kt
@@ -3,6 +3,7 @@ package com.cornellappdev.transit.util
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.net.toUri
 
 object IntentUtils {
@@ -12,13 +13,19 @@ object IntentUtils {
         if (launchIntent != null) {
             startActivity(launchIntent)
         } else {
+            val playStoreIntent = Intent(Intent.ACTION_VIEW, "market://details?id=$packageName".toUri())
+                .setPackage("com.android.vending")
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             try {
-                val playStoreIntent = Intent(Intent.ACTION_VIEW, "market://details?id=$packageName".toUri())
-                playStoreIntent.setPackage("com.android.vending")
                 startActivity(playStoreIntent)
-            } catch (e2: ActivityNotFoundException) {
-                startActivity(Intent(Intent.ACTION_VIEW, "https://play.google.com/store/apps/details?id=$packageName}".toUri()))
-
+            } catch (e: ActivityNotFoundException) {
+                val webStoreIntent = Intent(Intent.ACTION_VIEW, "https://play.google.com/store/apps/details?id=$packageName".toUri())
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                try {
+                    startActivity(webStoreIntent)
+                } catch (e2: ActivityNotFoundException) {
+                    Log.e("IntentUtils","no handler for play store web URL" ,e2)
+                }
             }
         }
     }
